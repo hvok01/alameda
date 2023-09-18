db38a7879a3ddf01abd027c38d975e3d985e44aa
hvok01 <leonelgomezrodriguez@hotmail.com>
2023-09-18 11:02:45 -0300

    Animation cards on page load

--- src/components/galery/Galery.tsx
+++ src/components/galery/Galery.tsx
@@ -1,16 +1,41 @@
 import Card from "../card/Card"
 import "./Galery.css"
 import { MockData } from "../../api/MockData"
-import { useEffect, useState } from "react"
+import { useEffect, useLayoutEffect, useRef, useState } from "react"
 import IProduct from "../../interfaces/IProduct"
+import gsap from 'gsap'
 
 export default function Galery() {
     const [data, setData] = useState<IProduct[]>([]);
+    const galeryCards = useRef(null);
 
     useEffect(() => {
-        const results: IProduct[] = MockData()
+        const results: IProduct[] = MockData();
         setData(results);
     }, [])
+
+    useLayoutEffect(() => {
+        const ctx = gsap.context(() => {
+            const tl = gsap.timeline()
+            animateCardsOnLoad(tl)
+        });
+      return () => ctx.revert()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    const animateCardsOnLoad = (tl: gsap.core.Timeline) => {
+        if(data && data.length > 0) {
+            const current: any = galeryCards.current;
+            const arrayOfCards : gsap.TweenTarget[]= current ? gsap.utils.toArray(current.children) : [];
+            arrayOfCards.forEach((card) => {
+                tl?.to(card, {
+                    opacity: 1,
+                    duration: 0.3,
+                    y: "+=4"
+                })
+            });
+        }
+    }
     
     return (
         <main className="galery-container">
@@ -21,17 +46,19 @@ export default function Galery() {
                     <li><a href="">Sale</a></li>
                 </ul>
             </div>
-            <div className="galery-cards-container">
+            <div className="galery-cards-container" ref={galeryCards}>
                 {
-                    data && data.map((item: IProduct) => {
-                        return  <Card 
-                                    id={item.id}
-                                    imgsrc={item.imgsrc} 
-                                    hoverimagesrc={item.hoverimagesrc} 
-                                    title={item.title} 
-                                    price={<p>{item.price} {item.offer ? <s>{item.offer}</s> : ""}</p>} 
-                                    offer={item.offer && item.offer.length > 0 ? true : false}
-                                />
+                    data && data.map((item: IProduct, key) => {
+                        return  <div key={key} style={{ opacity: 0 }}>
+                                    <Card 
+                                        id={item.id}
+                                        imgsrc={item.imgsrc} 
+                                        hoverimagesrc={item.hoverimagesrc} 
+                                        title={item.title} 
+                                        price={<p>{item.price} {item.offer ? <s>{item.offer}</s> : ""}</p>} 
+                                        offer={item.offer && item.offer.length > 0 ? true : false}
+                                    />
+                                </div>
                     })
                 }
             </div>
